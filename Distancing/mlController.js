7728a33232e80ceca17a056ecdff62026dac9a6e
josephnimal <joseph.nimal.fdo.l@gmail.com>
2020-05-21 13:42:57 +0530

    Adding Distancing

---
+++ Distancing/mlController.js
@@ -0,0 +1,52 @@
+var unityInstance = UnityLoader.instantiate("unityContainer", "Build/WEBGL.json", {onProgress: UnityProgress});
+
+let video;
+let poseNet;
+let pose;
+let skeleton;
+
+async function setup() {
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.hide();
+    //noCanvas();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('poseNet ready');
+}
+
+function gotPoses(poses) {
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
+    
+    extractData(pose);
+}
+}
+
+function draw(){
+  translate(width/2,0); // move to far corner
+  scale(-1.0,1.0);    // flip x-axis backwards
+  image(video, 0, 0, width/2, height/2);
+}
+
+function extractData(pose){
+    for (let i = 0; i < pose.keypoints.length; i++) {
+        let score = pose.keypoints[i].score;
+        let part = pose.keypoints[i].part;
+        let x = pose.keypoints[i].position.x;
+        let y = pose.keypoints[i].position.y;
+        let relX = map(x,0,640,-10,10)*-1;
+        let relY = map(y,0,480,-5,5)*-1;
+        sendMessage(part, score, relX, relY);
+    }
+}
+
+function sendMessage(part, score, x, y) {
+    if(unityInstance != null){
+        unityInstance.SendMessage("MovementController", part, score.toString().concat("|",x,"|",y));
+     }
+}
